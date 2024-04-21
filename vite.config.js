24e35fd569b8e8ed39ebc30cc7808a0889ca8f2c
3ae3ae <ogyshjsm@naver.com>
2024-04-21 15:20:44 +0900

    ads.txt 추가

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,14 @@ import { defineConfig } from "vite";
 
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
-  base: './',
+  base: "./",
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
         nested1: resolve(__dirname, "Content Guidelines.html"),
         nested2: resolve(__dirname, "createWord.html"),
+        nested3: resolve(__dirname, "ads.txt"),
       },
     },
   },
