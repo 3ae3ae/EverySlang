aac16a2e6d17400b5943b22b252ab49f028f969e
3ae3ae <ogyshjsm@naver.com>
2024-04-21 15:48:05 +0900

    ads.txt가 포함되지 않는 버그 수정. 패키지 이름 변경

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@ export default defineConfig({
         main: resolve(__dirname, "index.html"),
         nested1: resolve(__dirname, "Content Guidelines.html"),
         nested2: resolve(__dirname, "createWord.html"),
-        nested3: resolve(__dirname, "ads.txt"),
       },
     },
   },
